a64b73d8a7b3de83dadaee7070f09af101bb9994
Adrita Chakraborty <adichakrabort@gmail.com>
2022-11-24 23:21:31 +0530

    My Portfolio Website

---
+++ index.js
@@ -0,0 +1,23 @@
+<div class="infoCotainer">
+            <div class="devInfo">
+                <div class="hello"> Hi I am </div>
+                <div class="name"> Adrita Chakraborty </div>
+                <div class="about"> Developer,Coder,Dancer </div>
+                <div class="buttons">
+                    <a href="/cv.txt" target="_blank">
+                    <button>Download CV</button></a>
+                    <a href="/contact.html"> <button>Contact Me</button></a>
+                </div>
+            </div>
+            <div class="devPic"> <img src="photo.jpeg" alt="Adrita Chakraborty"></div>
+           </div>     
+
+
+
+           .sidebar{
+  transform: translate(-443px,0px);
+  position: absolute;
+
+}
+
+                                                                                                                                                                                                                                                                                                                                                                          
